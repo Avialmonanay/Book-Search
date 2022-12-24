5272cf31ffee03919ce6c0be47aaa3fbaffedcd4
Avialmonanay <108016215+Avialmonanay@users.noreply.github.com>
2022-12-24 02:42:37 -0700

    a good amount of server setup complete. Still need to troubleshoot why formData is not being reconized.

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,18 +1,20 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 
-import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const [login, { error, data }] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setUserFormData({ ...userFormData, [name]: value });
+    setUserFormData({ ...userFormData, [name]: value, });
   };
 
   const handleFormSubmit = async (event) => {
@@ -26,22 +28,17 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const { data } = await login({
+        variables: { ...userFormData },
+      })
+      Auth.login(data.login.token);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
     }
 
     setUserFormData({
-      username: '',
       email: '',
       password: '',
     });
--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations'
+
 import Auth from '../utils/auth';
+
 
 const SignupForm = () => {
   // set initial form state
-  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '', });
+  const [addUser, { error, data }] = useMutation(ADD_USER)
   // set state for form validation
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,17 +34,15 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      console.log("I made it here")
+      const { data } = await addUser({
+        variables: {...userFormData},
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
+      Auth.login(data.addUser.token)
+    } catch (e) {
+      console.error(e);
       setShowAlert(true);
     }
 
